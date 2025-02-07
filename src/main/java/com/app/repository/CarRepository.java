2c5ee48a889a15e60c044c542fefd4d4c8d7415a
Pratham Parashar <parasharpratham2001@gmail.com>
2025-02-07 11:45:06 +0530

    init commit

--- src/main/java/com/app/repository/CarRepository.java
+++ src/main/java/com/app/repository/CarRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-    @Query("Select c from Car c " + " JOIN c.brand b "+ "JOIN c.transmission t "+ " where b.name=:details or t.type=:details" )
+    @Query("Select c from Car c " + " JOIN c.brand b "+ "JOIN c.transmission t "+ "JOIN c.model m"+ " where b.name=:details or t.type=:detailsnor m.name=:details" )
     List<Car> searchCar(
             @Param("details")String details
     );
